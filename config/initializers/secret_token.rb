d22d944ab71697516af28b703b01a3d1ede4b728
pat <pat@affordableair.biz>
2013-06-11 17:17:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rat::Application.config.secret_token = '6c806e13c14c631a1a69405ac5531b43c86324dee646f027a381f7f222bd5718551637e016620deec14d5d237c3f845cad368b6f9d7fac2c690cfaceb0aa7bfb'
